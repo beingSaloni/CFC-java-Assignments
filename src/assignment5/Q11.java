6490efc331f61fd85907fe8d6c3b079b2cc7f972
beingSaloni <salonisaxena7777@gmail.com>
2021-02-28 23:10:37 +0530

    Assignment 5

--- src/assignment5/Q11.java
+++ src/assignment5/Q11.java
@@ -1,65 +1,63 @@
-//package assignment5;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//
-//public class Q11 {
-//    public static void main(String[] args) {
-//        int n = 4;
-//        int[][] board = new int[n][n];
-//        ArrayList<int[][]> list = new ArrayList<>();
-//       list= knightPlace(board,list, 0,0);
-//        for (int i = 0; i < list.size(); i++) {
-//            System.out.println(Arrays.deepToString(list.get(i)));
-//        }
-//
-//    }
-//
-//    private static ArrayList knightPlace(int[][] board, ArrayList<int[][]> list,int row,int col) {
-//        if(row>= board.length && col>= board.length){
-//            list.add(board);
-//            return list;
-//        }
-//        if(col >= board[0].length){
-//            row = row + 1;
-//            col=0;
-//        }
-//        for (int i = 1; i < board[0].length-1; i++) {
-//            if(isSafe(row ,col ,board)){
-//                board[row][col] = 1;
-//                knightPlace(board,list,row,col+i);
-//                board[row][col] =0;
-//
-//            }
-//
-//        }
-//
-//
-//
-//
-////        if(col == board[0].length){
-////            row = row +1;
-////        }
-////        if(isSafe(row ,col ,board)){
-////                    board[row][col] = 1;
-////                    knightPlace(board,list,row,col+1);
-////                }
-//
-//
-//        return list;
-//
-//    }
-//
-//    private static boolean isSafe(int row, int col, int[][] board) {
-//        int [] r = {2, 1, -1, -2, -2, -1, 1, 2};
-//      int [] c = {1, 2, 2, 1, -1, -2, -2, -1};
-//        for (int i = 0; i < 8; i++) {
-//            if( row + r[i] >= 0 &&  col + c[i] >=0 && row + r[i] < board.length && col + c[i] < board[0].length){
-//            if (board[row + r[i]][col +c[i]] == 1) {
-//                return false;
-//            }
-//
-//        }}
-//        return true;
-//    }
-//}
+package assignment5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Q11 {
+    public static void main(String[] args) {
+        int n = 5;
+        int[][] board = new int[n][n];
+        ArrayList<int[][]> list = new ArrayList<>();
+        knightPlace(board,list, 0,0);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(Arrays.deepToString(list.get(i)));
+        }
+
+    }
+
+    private static boolean knightPlace(int[][] board, ArrayList<int[][]> list,int row,int col) {
+        if(row>= board.length){
+            System.out.println(Arrays.deepToString(board));
+            return true;
+        }
+
+
+        for (int i = 0; i < board[0].length ; i++) {
+            if(isSafe(row ,col ,board)) {
+                if (col + i < board.length && row< board.length) {
+                    col = col + i;
+                    board[row][col] = 1;
+                    if(knightPlace(board, list, row, col + 1)){
+                        return true;
+                    }
+                    board[row][col] = 0;
+                }
+            }
+            if(col>=board[0].length-1 && row< board.length){
+                if(knightPlace(board,list,row +1,0)){
+                    return true;
+                }
+            }
+            if(i== board[0].length-1){
+                if(knightPlace(board,list,row,col+1)){
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+
+    private static boolean isSafe(int row, int col, int[][] board) {
+        int [] r = {2, 1, -1, -2, -2, -1, 1, 2};
+        int [] c = {1, 2, 2, 1, -1, -2, -2, -1};
+        for (int i = 0; i < 8; i++) {
+            if( row + r[i] >= 0 &&  col + c[i] >=0 && row + r[i] < board.length && col + c[i] < board[0].length){
+                if (board[row + r[i]][col +c[i]] == 1) {
+                    return false;
+                }
+
+            }}
+        return true;
+    }
+}
--- src/assignment5/Q12.java
+++ src/assignment5/Q12.java
@@ -1,4 +1,71 @@
 package assignment5;
 
+import java.util.Arrays;
+
 public class Q12 {
+    public static void main(String[] args) {
+        int[][] sudoku = { {3, 0, 6, 5, 0, 8, 4, 0, 0},
+                {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                {0, 0, 5, 2, 0, 6, 3, 0, 0} };
+        solveSudoku(sudoku,0,0);
+}
+
+    private static void solveSudoku(int[][] sudoku, int row ,int col) {
+        if(row== sudoku.length ){
+            System.out.println(Arrays.deepToString(sudoku));
+            return;
+        }
+        if(col== sudoku[0].length){
+
+            solveSudoku(sudoku, row+1, 0);
+            return;
+
+        }
+        if(sudoku[row][col] == 0){
+            for (int i = 1; i < 10; i++) {
+                if(isSafe(sudoku,row,col,i)){
+                    sudoku[row][col] = i;
+                    solveSudoku(sudoku,row ,col+1);
+                    sudoku[row][col] = 0;
+                }
+            }
+
+        }else {
+            solveSudoku(sudoku,row,col+1);
+        }
+
+
+    }
+    private static boolean isSafe(int[][] sudoku, int row, int col, int val) {
+        for (int r = 0; r < 9; r++) {
+            if(sudoku[r][col] == val){
+                return false;
+            }
+        }
+
+        for (int c = 0; c < 9; c++) {
+            if(sudoku[row][c] == val){
+                return false;
+            }
+        }
+
+        int box_r = (row/3)*3;
+        int box_c = (col/3)*3;
+
+        for (int r = box_r; r < box_r+3; r++) {
+            for (int c = box_c; c < box_c+3; c++) {
+                if(sudoku[r][c] == val){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
